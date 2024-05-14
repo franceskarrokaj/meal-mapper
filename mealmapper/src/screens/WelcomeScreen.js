6f4f5c07d0bdb4765dd7b807fb9a00af84fff49e
franceska <franceska.rrokajh@epitech.eu>
2024-05-14 11:37:06 +0200

    starting with other screens, until now i have home&welcome screen

--- mealmapper/src/screens/WelcomeScreen.js
+++ mealmapper/src/screens/WelcomeScreen.js
@@ -1,4 +1,4 @@
-import { View, Text } from 'react-native';
+import { View, Image, TouchableOpacity, Text} from 'react-native';
 import React, { useRef } from 'react';
 import { StatusBar } from 'react-native';
 import {
@@ -13,10 +13,63 @@ export default function WelcomeScreen() {
     const animation = useRef(null);
     const navigation = useNavigation();
     return (
-        <View style={{backgroundColor: '#ef0000', flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-            <Image source = {require("../../assets/images/plateofhealthyfood.png")}/>
-            <Text>Welcome MealMapper's friend!</Text>
+        <View style={{backgroundColor: '#ef0000', flex: 1, justifyContent: 'center', alignItems: 'center'}} >
+            <Image
+            source = {require("../../assets/images/login.png")}
+            style = {{
+                position: 'absolute',
+                width: wp('50%'), 
+                height: hp('15%')
+                }}
+            />
+            <StatusBar style='light' />
+            {/*Logo*/}
+            <View>
+                <LottieView
+                    autoPlay ref={animation}
+                    style = {{
+                    width: wp('40%'), 
+                    height: hp('5%')
+                }}
+                source={require('../../assets/images/login.png')}
+                />
+            </View>
+                {/*Title*/}
+            <View className='flex items-center space-y-2'>
+                <Text className='text-white font-extrabold tracking-widest'
+                style={{
+                    fontSize: hp('3.5%'),
+                    width: wp('45%'),
+                    height: hp('50%'),
+                }}
+                >
+                    Meal Mapper!
+                </Text>
 
+                <Text className='text white tracking-widest font-medium'
+                    style={{
+                        fontSize: hp('2.5%'),
+                    }}                
+                >
+
+                </Text>          
+            </View>
+
+            <View>
+                <TouchableOpacity
+                    style={{
+                        backgroundColor: '#fff',
+                        paddingVertical: hp('1.5%'),
+                        paddingHorizontal: hp('5%'),
+                        borderRadius: hp('1.5%'),
+                    }}
+                    onPress={() => navigation.navigate('Home')}
+                >
+                    <Text>
+                        Get started!
+                    </Text>
+                </TouchableOpacity>
+            </View>
         </View>
     );
 }
